db283f916228fe69517fa57f89b0dc7fa2e2df25
Alben Bustamante <albenbustamante@gmail.com>
2023-05-22 16:56:39 -0500

    configure: CORS

--- src/main/java/com/abb/expensemanager/config/SecurityConfig.java
+++ src/main/java/com/abb/expensemanager/config/SecurityConfig.java
@@ -7,6 +7,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 /**
  * The application security configuration.
@@ -15,9 +20,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    /**
+     * The main app security configuration.
+     *
+     * @param http the {@link HttpSecurity}.
+     * @return a {@link SecurityFilterChain} with the configuration.
+     * @throws Exception the error.
+     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests(request -> request
@@ -25,6 +39,24 @@ public class SecurityConfig {
                         .anyRequest().authenticated());
 
         return http.build();
+    }
+
+    /**
+     * The CORS configuration.
+     *
+     * @return a {@link UrlBasedCorsConfigurationSource}.
+     */
+    private CorsConfigurationSource corsConfigurationSource() {
+        final var config = new CorsConfiguration();
+
+        config.addAllowedOriginPattern("*");
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("*"));
+
+        final var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 
 }
