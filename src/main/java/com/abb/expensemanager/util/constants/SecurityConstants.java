d8db771521cce9ba97cfb9e5b9f1ac1a016ef448
Alben Bustamante <albenbustamante@gmail.com>
2023-05-21 19:33:38 -0500

    define: a white list for URIs that don't require authentication

---
+++ src/main/java/com/abb/expensemanager/util/constants/SecurityConstants.java
@@ -0,0 +1,13 @@
+package com.abb.expensemanager.util.constants;
+
+/**
+ * The main security constants.
+ */
+public final class SecurityConstants {
+
+    public static final String[] WHITE_LIST = {"/auth", "/auth/**"};
+
+    private SecurityConstants() {
+    }
+
+}
