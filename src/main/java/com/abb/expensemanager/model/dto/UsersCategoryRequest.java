a20a1212d63df6c172136afb764fa407eae13378
Alben Bustamante <albenbustamante@gmail.com>
2023-05-30 16:44:53 -0500

    add: users categories dto models

---
+++ src/main/java/com/abb/expensemanager/model/dto/UsersCategoryRequest.java
@@ -0,0 +1,7 @@
+package com.abb.expensemanager.model.dto;
+
+/**
+ * The users category's dto model as request.
+ */
+public record UsersCategoryRequest(int userId, int categoryId) {
+}
---
+++ src/main/java/com/abb/expensemanager/model/dto/UsersCategoryResponse.java
@@ -0,0 +1,10 @@
+package com.abb.expensemanager.model.dto;
+
+import java.time.LocalDateTime;
+
+/**
+ * The users category's dto model as response.
+ */
+public record UsersCategoryResponse(int userId, int categoryId, boolean isEnabled, LocalDateTime createdAt,
+                                    LocalDateTime updatedAt) {
+}
