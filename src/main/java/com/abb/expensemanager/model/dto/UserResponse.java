e483ee7587c3d903f7bb8e6b14fd3d87a4f67e93
Alben Bustamante <albenbustamante@gmail.com>
2023-05-30 13:05:11 -0500

    add: user categories at the response model

--- src/main/java/com/abb/expensemanager/model/dto/UserResponse.java
+++ src/main/java/com/abb/expensemanager/model/dto/UserResponse.java
@@ -5,11 +5,12 @@ import com.abb.expensemanager.util.enums.Role;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 /**
  * The user dto model as response.
  */
 public record UserResponse(int id, String name, String lastName, String username, String email, String phone,
-                           Gender gender, LocalDate birthday, Role role, boolean isEnabled, LocalDateTime createdAt,
-                           LocalDateTime updatedAt) {
+                           Gender gender, LocalDate birthday, Role role, Set<CategoryResponse> categories,
+                           boolean isEnabled, LocalDateTime createdAt, LocalDateTime updatedAt) {
 }
